b1ae6437f53bfabde21cb9160ca769b71c900006
Siyaf Khan <seyaffxh@gmail.com>
2026-03-26 13:53:33 -0700

    Removed comment

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const contactService = {
   transmitLead: async (data: { name: string; email: string; message: string }) => {
-    // Look how incredibly clean this is now. No full URL needed.
+   
     const response = await axios.post("/api/contact", data);
     return response.data;
   },
